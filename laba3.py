68c724990ff6e31c9afdf7d7684517c57486e9f2
alinasshka <113112262+alinasshka@users.noreply.github.com>
2022-12-20 12:06:13 +0300

    Create laba3.py

--- laba3.py
+++ laba3.py
@@ -1,39 +1,32 @@
 '''задание 1'''
-def SumList(x):
-    sum = 0
-    list = [int(input()) for i in range(x)]
-    for i in range(x):
-        sum += list[i]
-    print(sum)
-    print()
-
-SumList(10)
+numbers = []
+for i in range(0,10):
+    for i in input('вводи числа: ').split():
+        numbers.append(int(i))
+print(numbers)
+print(sum(numbers))
 
 '''задание 2'''
-def CountList(x):
-    count=0
-    list = [int(input()) for i in range(x)]
-    for i in list:
-        if i == 0:
-            count +=  1
-    print(count)
-
-CountList(10)
+numbers = []
+for i in input('вводи числа через пробел: ').split():
+    numbers.append(int(i))
+num_of_0 = numbers.count(0)
+print(num_of_0)
 
 '''задание 3'''
-print()
-
-x = int(input())
-for i in range(1, x+1):
-    for j in range(1, i+1):
-        print(j, end='')
-    print() 
+n = int(input('введи натуральное число: '))
+a = ''
+for i in range(1, n+1):
+    a = a + str(i)
+    print(a)
 
 '''задание 4'''
-x = int(input())
-for i in range(1, x+1):
-    for j in range(1, i+1):
-        print(j, end='')
-    for j in range(1, i)[::-1]:
-        print(j, end='')
-    print() 
+n = int(input('введи натуральное число: '))
+for i in range(1, n+1):
+    for a in range(1, n+1-i):
+        print(' ', end = '')
+    for b in range(1, i+1):
+        print(b, end = '')
+    for c in range(i-1, 0, -1):
+        print(c, end = '')
+    print()
